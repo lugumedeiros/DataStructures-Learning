8d16ca8b63c4c86aeb649ff05343654997b9765a
Luiz Gustavo de Medeiros <lugumedeiros@gmail.com>
2025-05-10 14:28:53 -0300

    update DFT to print edge types

--- Data_structures/depth_first_tree.py
+++ Data_structures/depth_first_tree.py
@@ -3,13 +3,15 @@ class Node:
         self.key = key
         self.start = None
         self.end = None
-        self.paren = None
+        self.parent = None
 
 class DFT:
     count = 1
     def __init__(self, graph):
         self.node_map, self.edge_map = self._get_all_nodes(graph)
-        value = self.run()
+        self.graph = graph
+        self.run()
+        self.classify_edges()
         # self.count = 1
         pass
     
@@ -35,15 +37,21 @@ class DFT:
 
         return node_map, edge_map
 
-    def run(self, start=1, store=1):
+    def run(self, start=1, store=1, parent=1):
         node = self.node_map[start]
         node.start = self.count
+        
+        if node.parent is not None:
+            return
+        else:
+            node.parent = parent
+
         self.count += 1
 
         # Main tree        
         for edge in self.edge_map[node.key]:
             if edge.end is None and edge.start is None:
-                self.run(edge.key, start)
+                self.run(edge.key, start, node.key)
 
         node.end = self.count
         self.count += 1
@@ -58,6 +66,40 @@ class DFT:
         for _, node in self.node_map.items():
             msg = f"key: {node.key}, start: {node.start}, end:{node.end}"
             print(msg)
+
+        print(f"Tree edges: ", self.tree)
+        print(f"Forward edges: ", self.forward)
+        print(f"Backward edges: ", self.backward)
+        print(f"Cross edges: ", self.cross)
+
+
+    def classify_edges(self):
+        self.forward = []
+        self.backward = []
+        self.cross = []
+        self.tree = []
+        
+        for edge in self.graph:
+            root_key, end_key = edge
+            
+            root_node = self.node_map[root_key]
+            root_start = root_node.start
+            root_end = root_node.end
+
+            end_node = self.node_map[end_key]
+            end_parent = end_node.parent
+            end_start = end_node.start
+            end_end = end_node.end
+
+            # Logic
+            if root_start > end_end:
+                self.cross.append(edge)
+            elif root_key == end_parent:
+                self.tree.append(edge)
+            elif end_end < root_end:
+                self.forward.append(edge)
+            else:
+                self.backward.append(edge)
 
 
 if __name__ == "__main__":
